e993f6083c05b99f121baf4b0f278a31062ceeb7
sneh913 <snehalmane083@gmail.com>
2026-04-29 14:12:14 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-cd /home/ec2-user/app
+cd /home/ec2-user/app/api-blsheet
 
 pm2 delete all || true
-pm2 start --interpreter tsx src/index.ts --name blsheet-backend
+pm2 start "npx tsx api/index.ts" --name blsheet-backend
 pm2 save
