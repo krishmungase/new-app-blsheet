be95cdb260d9996b7167e50308ed8affec54c18e
sneh913 <snehalmane083@gmail.com>
2026-04-30 09:12:04 +0530

    Update index.ts

--- api-blsheet/src/index.ts
+++ api-blsheet/src/index.ts
@@ -35,6 +35,8 @@ app.get(
 // -------------------- SERVER START --------------------
 const PORT = process.env.PORT || 5555
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+export default app
